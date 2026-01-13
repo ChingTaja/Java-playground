e19830a28ed417db52779f8e6c9cdd10f1f597ad
Taja <taja526526@gmail.com>
2026-01-14 00:40:07 +0800

    feat: function

---
+++ Base/KeywordsAndExpression.java
@@ -0,0 +1,61 @@
+package Base;
+public class KeywordsAndExpression {
+    public static void main(String[] args) {
+
+        /* ch44 Expression */
+
+        double kilometer = (100 * 1.5566);
+        // 100 * 1.5566 -> expression 算出一個值
+        // double kilometer = (100 * 1.5566) -> statement 執行一個動作的指令
+
+        int highscroe = 50;
+
+        int health = 100;
+
+        if ((health < 25) && (highscroe > 100)) {
+            highscroe = highscroe - 1000;
+        }
+
+        /* ch45 Statement */
+        int myTest = 50;
+        myTest++;
+        myTest--;
+
+        System.out.println("This is test");
+
+        /* ch46 if-then-else */
+        boolean gameOver = true;
+        int score = 100;
+        int levelComplete = 5;
+        int bonus = 100;
+        
+        int highScroe = calculateScore(gameOver, score, levelComplete, bonus);
+        System.out.println("Your high Scroe was:" + highScroe);
+
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score was less than 5000");
+        } else if (score < 1000) {
+            System.out.println("Your score was less than 1000");
+
+        } else {
+            System.out.println("Got here");
+        }
+    }
+    
+    /* ch48 reuse code */
+    /* ch49 return value */
+    public static int calculateScore(boolean gameOver, int score, int levelComplete, int bonus) {
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelComplete * bonus);
+            finalScore += 1000;
+        }
+        
+        return finalScore;
+    }
+    /* ch50 */
+    /* ch51 */
+    /* ch52 */
+
+}
