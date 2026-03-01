73dba2ffa87a0f2958caa38924357b4a776f31f8
Taja <taja526526@gmail.com>
2026-03-01 17:56:02 +0800

    feat: getter setter this

--- Base/Basic.java
+++ Base/Basic.java
@@ -5,8 +5,20 @@ public class Basic {
         Car car = new Car(); // 告一個型別為 Car 的變數
 
         // car.make = "Porsche"; // Error!
-        System.out.println("make =" + car.getColor()); 
+        car.setMake("Porsche");
+        car.setModel("Carrera");
+        car.setDoors(2);
+        car.setConvertible(true);
+        car.setColor("black");
+        System.out.println("make =" + car.getColor());
 
         car.describeCar(); // 0 door null, null, null
     }
+    
+    /*
+     * 這裡有一個重要區別：未初始化變數 與 指向 null 的變數。
+     * 
+     * 未初始化變數： 會在 編譯時（compile-time） 發生錯誤
+     * 指向 null 的變數：編譯不會報錯，但在執行時會 丟出例外
+     */
 }
--- Base/Car.java
+++ Base/Car.java
@@ -7,13 +7,11 @@ public class Car {
     private int doors;
     private boolean convertible;
 
-    public String getMake() {
+    public String getMake() {        
         return make;
     }
 
-    
-
-    // Java Extension Pack 可以自動生成 getter 方法
+    // Java Extension Pack 可以自動生成 getter & setter 方法
 
     public String getModel() {
         return model;
@@ -23,17 +21,57 @@ public class Car {
         return color;
     }
 
-
-
     public int getDoors() {
         return doors;
     }
 
-
-
     public boolean isConvertible() {
         return convertible;
     }
+
+    public void setMake(String make) {
+        // make = make;
+        if (make == null)
+            make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake) {
+            case "holden", "porsche", "teska" -> this.make = make;
+            default -> {
+                this.make = "Unsupported";
+            }
+        }
+        /*
+         * 我有兩個不同的變數都叫做 make：
+         * 
+         * 1. 欄位（field）：`private String make`
+         * 2. 方法參數（parameter）：`String make`
+         * 
+         * 在程式裡，實際上不是把欄位 make 設為參數 make，
+         * 而是把 參數 make 指派給自己。
+         * 
+         * 要做的是：
+         * 把 private String make 這個欄位，更新成傳入方法的參數值
+         * 
+         * ==> 使用 this
+         */
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+    
 
     // 和欄位不同的是，方法通常會設為 public
     // 因為我們希望提供一個方式，讓使用者可以與物件進行互動
