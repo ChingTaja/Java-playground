23e9cdc5afa1d3c18bf994443c75427f5f17a90b
Taja <taja526526@gmail.com>
2026-03-20 00:20:49 +0800

    feat: Polymorphism

---
+++ Polymorphism/Main.java
@@ -0,0 +1,10 @@
+package Polymorphism;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Movie theMovie = new Adventure("Star Wars");
+        theMovie.watchMovie();
+    }
+}
