85796fe23968087e172291bba15a1c15493600f9
Taja <taja526526@gmail.com>
2026-03-24 23:50:12 +0800

    feat: array

---
+++ Array/dev/lpa/Main.java
@@ -0,0 +1,27 @@
+package Array.dev.lpa;
+
+public class Main {
+    public static void main(String[] args) {
+        // 一旦陣列實例化之後，你就無法改變陣列的大小
+        // 你不能增加或刪除元素
+        // 在這個範例裡你只能對這 10 個元素中的其中一個進行賦值
+        int[] myIntArray = new int[10];
+        myIntArray[5] = 50;
+
+        double[] myDoubleArray = new double[10];
+        myDoubleArray[2] = 3.5;
+        System.out.println(myDoubleArray[2]);
+
+        int[] firstTen = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println("first = " + firstTen[0]);
+        int arrayLength = firstTen.length;
+        System.out.println("length of array" + arrayLength);
+        System.out.println("last" + firstTen[arrayLength -1]);
+
+        int[] newArray;
+        newArray = new int[] { 5, 4, 3, 2, 1 };
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.println(newArray[i] + " ");
+        }
+    }
+}
---
+++ Array/intro.md
@@ -0,0 +1,12 @@
+An array is a data structure that allows you to store a sequence of values, all of the **same type**.
+
+An array is a special class in Java. It's still a class.
+
+The array, like all other classes, ultimately inherits from 
+```java.lang.Object```
+it's still a class.
+
+```java
+int [ ] intergerArray --> more common
+String courseList[]
+```
