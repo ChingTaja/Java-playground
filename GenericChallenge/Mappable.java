c7256a74e930ff1347f913b927fcc032f6152177
Taja <taja526526@gmail.com>
2026-04-25 13:01:23 +0800

    feat: challenge

---
+++ GenericChallenge/Mappable.java
@@ -0,0 +1,55 @@
+package GenericChallenge;
+
+import java.util.Arrays;
+
+public interface Mappable {
+    void render();
+
+    // 在 Java 8 之後：  介面可以有 static 方法
+    static double[] StringToLoaction(String location) {
+        var split = location.split(",");
+        double lat = Double.valueOf(split[0]);
+        double lng = Double.valueOf(split[1]);
+
+        return new double[] { lat, lng };
+    }
+}
+
+abstract class Point implements Mappable {
+    private double[] location = new double[2];
+
+    
+
+    @Override
+    public void render() {
+        System.out.println("Render" + this + "as POINT (" + location() + ")");
+    }
+
+    // Encapsulation
+    private String location() {
+        return Arrays.toString(location);
+    }
+}
+
+abstract class Line implements Mappable {
+
+    @Override
+    public void render() {
+        System.out.println("Render" + this + "as Lines (" + locations() + ")");
+    }
+
+    private double[][] locations;
+
+    public Line(String... locations) {
+        this.locations = new double[locations.length][];
+        int index = 0;
+        for (var l : locations) {
+            this.locations[index++] = Mappable.StringToLoaction(l);
+        }
+    }
+
+    private String locations() {
+        // 把「多維陣列（尤其是巢狀陣列）」轉成好讀的字串
+        return Arrays.deepToString(locations);
+    }
+}
